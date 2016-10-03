45e57cf9022c4cb4d5efe047a86e80e3aae2a632
BenGoBlue05 <bplewis5@gmail.com>
2016-10-03 09:16:48 -0500

    Initial commit

---
+++ app/src/main/java/com/scholar/dollar/android/dollarscholarbenlewis/CollegeMainFragment.java
@@ -0,0 +1,23 @@
+package com.scholar.dollar.android.dollarscholarbenlewis;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bplewis5 on 10/3/16.
+ */
+
+public class CollegeMainFragment extends Fragment{
+    public CollegeMainFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/scholar/dollar/android/dollarscholarbenlewis/MajorFragment.java
@@ -0,0 +1,23 @@
+package com.scholar.dollar.android.dollarscholarbenlewis;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bplewis5 on 10/3/16.
+ */
+
+public class MajorFragment extends android.support.v4.app.Fragment{
+
+    public MajorFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
